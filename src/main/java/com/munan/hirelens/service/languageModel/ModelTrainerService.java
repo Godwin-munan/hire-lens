75b56f7d48d7264c2208be5e3d79f33f275cc3d9
Godwin-munan <godwinjethro@gmail.com>
2025-01-13 17:26:46 +0100

    update to model training service

--- src/main/java/com/munan/hirelens/service/languageModel/ModelTrainerService.java
+++ src/main/java/com/munan/hirelens/service/languageModel/ModelTrainerService.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.Getter;
+import opennlp.tools.namefind.TokenNameFinderModel;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
@@ -16,49 +18,55 @@ public class ModelTrainerService {
 
     private final Map<ModelTypes, ModelTrainer> modelTrainerMap = new HashMap<>();
 
+    @Getter
+    private final Map<ModelTypes, TokenNameFinderModel> modelMap = new ConcurrentHashMap<>();
+
     public ModelTrainerService(NLPProperties nlpProperties, ResourceLoader resourceLoader) {
         validatePaths(nlpProperties);
+
         // Initialize model trainers dynamically based on ModelTypes
         modelTrainerMap.put(
             ModelTypes.PERSON,
             new ModelTrainer(
-                nlpProperties.getTrainingModel().getPath().getPerson(),
-                nlpProperties.getTrainingData().getPath().getPerson(),
+                nlpProperties.getTrainingData().getClassPath().getPerson(),
+                nlpProperties.getTrainingData().getFilePath().getPerson(),
+                nlpProperties.getTrainingModel().getClassPath().getPerson(),
+                nlpProperties.getTrainingModel().getFilePath().getPerson(),
                 resourceLoader
             )
         );
-        modelTrainerMap.put(
-            ModelTypes.EDUCATION,
-            new ModelTrainer(
-                nlpProperties.getTrainingModel().getPath().getEducation(),
-                nlpProperties.getTrainingData().getPath().getEducation(),
-                resourceLoader
-            )
-        );
-        modelTrainerMap.put(
-            ModelTypes.COMPANY,
-            new ModelTrainer(
-                nlpProperties.getTrainingModel().getPath().getCompany(),
-                nlpProperties.getTrainingData().getPath().getCompany(),
-                resourceLoader
-            )
-        );
-        modelTrainerMap.put(
-            ModelTypes.LOCATION,
-            new ModelTrainer(
-                nlpProperties.getTrainingModel().getPath().getLocation(),
-                nlpProperties.getTrainingData().getPath().getLocation(),
-                resourceLoader
-            )
-        );
-        modelTrainerMap.put(
-            ModelTypes.SKILLS,
-            new ModelTrainer(
-                nlpProperties.getTrainingModel().getPath().getSkills(),
-                nlpProperties.getTrainingData().getPath().getLocation(),
-                resourceLoader
-            )
-        );
+        //        modelTrainerMap.put(
+        //            ModelTypes.EDUCATION,
+        //            new ModelTrainer(
+        //                nlpProperties.getTrainingModel().getPath().getEducation(),
+        //                nlpProperties.getTrainingData().getPath().getEducation(),
+        //                resourceLoader
+        //            )
+        //        );
+        //        modelTrainerMap.put(
+        //            ModelTypes.COMPANY,
+        //            new ModelTrainer(
+        //                nlpProperties.getTrainingModel().getPath().getCompany(),
+        //                nlpProperties.getTrainingData().getPath().getCompany(),
+        //                resourceLoader
+        //            )
+        //        );
+        //        modelTrainerMap.put(
+        //            ModelTypes.LOCATION,
+        //            new ModelTrainer(
+        //                nlpProperties.getTrainingModel().getPath().getLocation(),
+        //                nlpProperties.getTrainingData().getPath().getLocation(),
+        //                resourceLoader
+        //            )
+        //        );
+        //        modelTrainerMap.put(
+        //            ModelTypes.SKILLS,
+        //            new ModelTrainer(
+        //                nlpProperties.getTrainingModel().getPath().getSkills(),
+        //                nlpProperties.getTrainingData().getPath().getLocation(),
+        //                resourceLoader
+        //            )
+        //        );
     }
 
     /**
@@ -70,8 +78,8 @@ public class ModelTrainerService {
             ModelTrainer trainer = modelTrainerMap.get(modelType);
 
             if (Objects.nonNull(trainer)) {
-                // TODO: LOG - Training model for: {modelType.name()}
-                trainer.ensureModel(modelType);
+                TokenNameFinderModel model = trainer.ensureModel(modelType);
+                modelMap.put(modelType, model);
                 // TODO: LOG - Model trained for entity: {modelType.name()}
             } else {
                 // TODO: LOG - No trainer found for model type: {modelType.name()}
@@ -81,34 +89,36 @@ public class ModelTrainerService {
 
     private void validatePaths(NLPProperties nlpProperties) {
         if (
-            Objects.isNull(nlpProperties.getTrainingModel().getPath().getPerson()) ||
-            Objects.isNull(nlpProperties.getTrainingData().getPath().getPerson())
+            Objects.isNull(nlpProperties.getTrainingData().getClassPath().getPerson()) ||
+            Objects.isNull(nlpProperties.getTrainingData().getFilePath().getPerson()) ||
+            Objects.isNull(nlpProperties.getTrainingModel().getClassPath().getPerson()) ||
+            Objects.isNull(nlpProperties.getTrainingModel().getFilePath().getPerson())
         ) {
             throw new IllegalStateException("Model or training data path is null for model type: PERSON");
         }
-        if (
-            Objects.isNull(nlpProperties.getTrainingModel().getPath().getEducation()) ||
-            Objects.isNull(nlpProperties.getTrainingData().getPath().getEducation())
-        ) {
-            throw new IllegalStateException("Model or training data path is null for model type: EDUCATION");
-        }
-        if (
-            Objects.isNull(nlpProperties.getTrainingModel().getPath().getCompany()) ||
-            Objects.isNull(nlpProperties.getTrainingData().getPath().getCompany())
-        ) {
-            throw new IllegalStateException("Model or training data path is null for model type: COMPANY");
-        }
-        if (
-            Objects.isNull(nlpProperties.getTrainingModel().getPath().getLocation()) ||
-            Objects.isNull(nlpProperties.getTrainingData().getPath().getLocation())
-        ) {
-            throw new IllegalStateException("Model or training data path is null for model type: LOCATION");
-        }
-        if (
-            Objects.isNull(nlpProperties.getTrainingModel().getPath().getSkills()) ||
-            Objects.isNull(nlpProperties.getTrainingData().getPath().getSkills())
-        ) {
-            throw new IllegalStateException("Model or training data path is null for model type: SKILLS");
-        }
+        //        if (
+        //            Objects.isNull(nlpProperties.getTrainingModel().getPath().getEducation()) ||
+        //            Objects.isNull(nlpProperties.getTrainingData().getPath().getEducation())
+        //        ) {
+        //            throw new IllegalStateException("Model or training data path is null for model type: EDUCATION");
+        //        }
+        //        if (
+        //            Objects.isNull(nlpProperties.getTrainingModel().getPath().getCompany()) ||
+        //            Objects.isNull(nlpProperties.getTrainingData().getPath().getCompany())
+        //        ) {
+        //            throw new IllegalStateException("Model or training data path is null for model type: COMPANY");
+        //        }
+        //        if (
+        //            Objects.isNull(nlpProperties.getTrainingModel().getPath().getLocation()) ||
+        //            Objects.isNull(nlpProperties.getTrainingData().getPath().getLocation())
+        //        ) {
+        //            throw new IllegalStateException("Model or training data path is null for model type: LOCATION");
+        //        }
+        //        if (
+        //            Objects.isNull(nlpProperties.getTrainingModel().getPath().getSkills()) ||
+        //            Objects.isNull(nlpProperties.getTrainingData().getPath().getSkills())
+        //        ) {
+        //            throw new IllegalStateException("Model or training data path is null for model type: SKILLS");
+        //        }
     }
 }
--- src/main/java/com/munan/hirelens/service/languageModel/NameLangService.java
+++ src/main/java/com/munan/hirelens/service/languageModel/NameLangService.java
@@ -6,28 +6,46 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.Span;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NameLangService {
-    //    private static final TokenNameFinderModel MODEL;
 
-    //    static {
-    //        try (InputStream modelStream = NameLangService.class.getResourceAsStream("/en-ner-person.bin")) {
-    //            MODEL = new TokenNameFinderModel(modelStream);
-    //        } catch (IOException e) {
-    //            throw new RuntimeException("Failed to load name finder model", e);
-    //        }
-    //    }
+    //private static final TokenNameFinderModel MODEL;
+    private static final TokenNameFinderModel NAME_FINDER_MODEL;
+    private static final TokenizerModel TOKENIZER_MODEL;
 
-    //    public String extractNames(String text) {
-    //        NameFinderME nameFinder = new NameFinderME(MODEL);
-    //        String[] tokens = text.split("\\s+"); // Replace with TokenizerME for better accuracy
-    //        Span[] nameSpans = nameFinder.find(tokens);
-    //
-    //        return Arrays.stream(nameSpans)
-    //            .map(span -> String.join(" ", Arrays.copyOfRange(tokens, span.getStart(), span.getEnd())))
-    //            .collect(Collectors.joining(", "));
-    //    }
+    static {
+        try (
+            InputStream nameFinderModelStream = NameLangService.class.getResourceAsStream("/models/en-ner-person.bin");
+            InputStream tokenizerModelStream = NameLangService.class.getResourceAsStream("/models/en-nr-lang-tokens.bin")
+        ) {
+            if (nameFinderModelStream == null) {
+                throw new RuntimeException("Name Finder model file not found in classpath: /models/en-ner-person.bin");
+            }
+            if (tokenizerModelStream == null) {
+                throw new RuntimeException("Tokenizer model file not found in classpath: /models/en-ner-person.bin");
+            }
+
+            NAME_FINDER_MODEL = new TokenNameFinderModel(nameFinderModelStream);
+            TOKENIZER_MODEL = new TokenizerModel(tokenizerModelStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load models", e);
+        }
+    }
+
+    public String extractNames(String text) {
+        TokenizerME tokenizer = new TokenizerME(TOKENIZER_MODEL);
+        String[] tokens = tokenizer.tokenize(text);
+
+        NameFinderME nameFinder = new NameFinderME(NAME_FINDER_MODEL);
+        Span[] nameSpans = nameFinder.find(tokens);
+
+        return Arrays.stream(nameSpans)
+            .map(span -> String.join(" ", Arrays.copyOfRange(tokens, span.getStart(), span.getEnd())))
+            .collect(Collectors.joining(", "));
+    }
 }
