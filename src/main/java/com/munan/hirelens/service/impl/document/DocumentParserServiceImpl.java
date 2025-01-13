50628f449603ce925ac9b72031f5a786d228a6fc
Godwin-munan <godwinjethro@gmail.com>
2025-01-13 17:24:46 +0100

    bug fix on document uploading endpoint v1

--- src/main/java/com/munan/hirelens/service/impl/document/DocumentParserServiceImpl.java
+++ src/main/java/com/munan/hirelens/service/impl/document/DocumentParserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.munan.hirelens.service.impl.document;
 
 import com.munan.hirelens.service.document.DocumentParserService;
+import com.munan.hirelens.service.languageModel.NameLangService;
 import com.munan.hirelens.utils.regex.RegexUtils;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,24 +10,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import lombok.RequiredArgsConstructor;
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@RequiredArgsConstructor
 public class DocumentParserServiceImpl implements DocumentParserService {
+
+    private final NameLangService nameLangService;
 
     @Override
     public Map<String, Object> parseDoc(MultipartFile file) throws IOException, TikaException {
         // Step 1: Extract text from file
         String text = extractTextFromFile(file);
-        System.out.println("################################File String : " + text);
+        //System.out.println("################################File String : " + text);
 
         // Step 2: Parse details using regex or NLP
-        Map<String, Object> extractedData = extractDetailsFromText(text);
 
-        return extractedData;
+        return extractDetailsFromText(text);
     }
 
     private String extractTextFromFile(MultipartFile file) throws IOException, TikaException {
@@ -40,7 +44,7 @@ public class DocumentParserServiceImpl implements DocumentParserService {
         }
     }
 
-    private Map<String, Object> extractDetailsFromText(String text) {
+    private Map<String, Object> extractDetailsFromText(String text) throws IOException {
         Map<String, Object> details = new HashMap<>();
 
         // Example regex for basic data extraction
@@ -55,8 +59,14 @@ public class DocumentParserServiceImpl implements DocumentParserService {
         //            details.put("phone", phoneMatcher.group());
         //        }
 
+        String extractedNames = nameLangService.extractNames(text);
+
+        // Return results as a map
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("PERSON", extractedNames);
+
         // Add more regex or NLP for names, skills, etc.
 
-        return details;
+        return resultMap;
     }
 }
