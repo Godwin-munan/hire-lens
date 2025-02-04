b5097457d54cef5f3cdf40fefd7128f1c8520a20
Godwin-munan <godwinjethro@gmail.com>
2025-02-04 22:30:27 +0100

    Implemented Mockito spy test to model loading and training to spring-boot application context on startup

--- gateway/src/test/java/com/munan/gateway/utils/modeltrainer/ModelTrainerTest.java
+++ gateway/src/test/java/com/munan/gateway/utils/modeltrainer/ModelTrainerTest.java
@@ -1,6 +1,6 @@
 package com.munan.gateway.utils.modeltrainer;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 public class ModelTrainerTest {
@@ -28,6 +29,12 @@ public class ModelTrainerTest {
 
     @Mock
     private ResourceLoader resourceLoader;
+
+    @Mock
+    private Resource modelResource;
+
+    @Mock
+    private Resource dataResource;
 
     @Mock
     private InputStream mockModelInputStream;
@@ -74,5 +81,63 @@ public class ModelTrainerTest {
         TokenNameFinderModel result = spyTrainer.ensureModel(ModelTypes.PERSON);
         assertNotNull(result);
         verify(spyTrainer, times(1)).trainNewModel(eq(ModelTypes.PERSON), any());
+    }
+
+    @Test
+    void testEnsureModelLoadsExistingModel() throws IOException {
+        // Mock resolveClasspathResource to return model input stream
+        when(resourceLoader.getResource(MODEL_CLASS_PATH)).thenReturn(modelResource);
+        when(resourceLoader.getResource(TRAINING_DATA_CLASS_PATH)).thenReturn(dataResource);
+
+        Optional<InputStream> optionalModelStream = Optional.of(mockModelInputStream);
+        Optional<InputStream> optionalTrainingDataStream = Optional.of(mockTrainingDataInputStream);
+
+        ModelTrainer spyTrainer = spy(modelTrainer);
+
+        doReturn(optionalTrainingDataStream).when(spyTrainer).resolveClasspathResource(TRAINING_DATA_CLASS_PATH);
+        doReturn(optionalModelStream).when(spyTrainer).resolveClasspathResource(MODEL_CLASS_PATH);
+        doReturn(mockModel).when(spyTrainer).loadExistingModel(mockModelInputStream);
+
+        TokenNameFinderModel result = spyTrainer.ensureModel(ModelTypes.PERSON);
+        assertNotNull(result);
+        verify(spyTrainer, times(1)).loadExistingModel(mockModelInputStream);
+    }
+
+    @Test
+    void testEnsureModelModelClassPathIsNull() throws IOException {
+        ModelTrainer modelTrainer = new ModelTrainer(
+            TRAINING_DATA_CLASS_PATH,
+            TRAINING_DATA_FILE_PATH,
+            null,
+            MODEL_FILE_PATH,
+            resourceLoader
+        );
+        ModelTrainer spyTrainer = spy(modelTrainer);
+
+        // Assert that ensureModel() throws an IllegalStateException
+        IllegalStateException thrownException = assertThrows(IllegalStateException.class, () -> {
+            spyTrainer.ensureModel(ModelTypes.PERSON);
+        });
+
+        // Verify the exception message
+        assertEquals("Model file path is null for model type: " + ModelTypes.PERSON, thrownException.getMessage());
+    }
+
+    @Test
+    void testEnsureModelTrainingDataClassPathIsNull() throws IOException {
+        when(resourceLoader.getResource(TRAINING_DATA_CLASS_PATH)).thenReturn(dataResource);
+        ModelTrainer spyTrainer = spy(modelTrainer);
+
+        Optional<InputStream> optionalTrainingDataStream = Optional.empty();
+
+        doReturn(optionalTrainingDataStream).when(spyTrainer).resolveClasspathResource(TRAINING_DATA_CLASS_PATH);
+
+        // Assert that ensureModel() throws an IllegalStateException
+        IllegalStateException thrownException = assertThrows(IllegalStateException.class, () -> {
+            spyTrainer.ensureModel(ModelTypes.PERSON);
+        });
+
+        // Verify the exception message
+        assertEquals("Training data input stream is null for path: " + TRAINING_DATA_CLASS_PATH, thrownException.getMessage());
     }
 }
