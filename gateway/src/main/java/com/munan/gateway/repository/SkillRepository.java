718daf60727d112bfd087789af23e66dd74a261f
Godwin-munan <godwinjethro@gmail.com>
2025-02-14 12:50:20 +0100

    Added custom repository methods for skill and parse-document repos

--- gateway/src/main/java/com/munan/gateway/repository/SkillRepository.java
+++ gateway/src/main/java/com/munan/gateway/repository/SkillRepository.java
@@ -1,8 +1,11 @@
 package com.munan.gateway.repository;
 
 import com.munan.gateway.domain.document.Skill;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SkillRepository extends JpaRepository<Skill, Long> {}
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+    Optional<Skill> findByName(String name);
+}
